d2bd895053d6d2cd31f6b38b43f4676747d7ce45
vlaforet <victor.laforet@inria.fr>
2022-10-26 15:03:35 +0200

    Progressive contention benchmark

--- bmarks/scheduling.c
+++ bmarks/scheduling.c
@@ -16,20 +16,27 @@
 #include "lock_if.h"
 #include "hybridlock.h"
 
-#define DEFAULT_NB_THREADS 1
+#define DEFAULT_NB_THREADS 10
 #define DEFAULT_USE_LOCKS 1
-#define DEFAULT_LAUNCH_DELAY 1000000000
-#define DEFAULT_CS_DELAY 1000000000
+#define DEFAULT_LAUNCH_DELAY_MS 1000
+#define DEFAULT_CS_CYCLES 10000
 
 #define XSTR(s) STR(s)
 #define STR(s) #s
 
+#define DURATION(t1, t2) (t2.tv_sec * 1000 + t2.tv_usec / 1000) - (t1.tv_sec * 1000 + t1.tv_usec / 1000)
+
 /* ################################################################### *
  * GLOBALS
  * ################################################################### */
-int use_locks;
-int cs_delay;
-int stop = 0;
+
+int use_locks = DEFAULT_USE_LOCKS;
+int cs_cycles;
+
+struct timeval start;
+unsigned long cs_count = 0;
+_Atomic int thread_count = 0;
+int needed_threads;
 
 __thread uint32_t phys_id;
 lock_global_data the_lock;
@@ -54,63 +61,72 @@ typedef struct thread_data
 void *test(void *data)
 {
     thread_data_t *d = (thread_data_t *)data;
-    phys_id = the_cores[d->id];
-
-    /* local initialization of locks */
-    init_lock_local(phys_id, &the_lock, &(local_th_data[d->id]));
-    printf("Starting thread %d\n", d->id);
+    init_lock_local(INT_MAX, &the_lock, &(local_th_data[d->id]));
+    thread_count++;
+    int stop = 0;
 
     while (!stop)
     {
-        acquire_write(&(local_th_data[d->id]), &the_lock);
-        cpause(cs_delay);
-        release_write(&(local_th_data[d->id]), &the_lock);
+        if (use_locks)
+            acquire_write(&(local_th_data[d->id]), &the_lock);
 
-        cpause(cs_delay);
+        cs_count++;
+        cpause(cs_cycles);
+
+        if (thread_count > needed_threads)
+        {
+            thread_count--;
+            stop = 1;
+        }
+
+        if (use_locks)
+            release_write(&(local_th_data[d->id]), &the_lock);
+
+        cpause(cs_cycles);
     }
 
     free_lock_local(local_th_data[d->id]);
     return NULL;
 }
 
-void catcher(int sig)
+void measurement()
 {
-    static int nb = 0;
+    static unsigned long last_cs_count = 0;
+    static struct timeval last_measurement, current;
+    static unsigned int diff;
+    static double duration;
 
-#ifdef USE_HYBRIDLOCK_LOCKS
-    if (nb == 0)
-    {
-        printf("Hybrid Lock: Blocking\n");
-        set_blocking(&the_lock);
-        nb++;
+    if (thread_count <= 0)
         return;
-    }
-#endif
 
-    printf("CAUGHT SIGNAL %d\n", sig);
-    if (++nb >= 3)
-        exit(1);
+    if (last_measurement.tv_sec == 0 && last_measurement.tv_usec == 0)
+        last_measurement = start;
+
+    gettimeofday(&current, NULL);
+    duration = DURATION(last_measurement, current);
+    diff = cs_count - last_cs_count;
+
+    last_measurement = current;
+    last_cs_count += diff;
+
+    if (diff > 0)
+        printf("%d, %f, %f\n", thread_count, (double)DURATION(start, current), diff / duration);
 }
 
 int main(int argc, char **argv)
 {
     set_cpu(the_cores[0]);
+    int i, c;
+    int max_nb_threads = DEFAULT_NB_THREADS;
+    int launch_delay = DEFAULT_LAUNCH_DELAY_MS;
+
     struct option long_options[] = {
-        // These options don't set a flag
         {"help", no_argument, NULL, 'h'},
-        {"cs-delay", required_argument, NULL, 'c'},
+        {"cs-cycles", required_argument, NULL, 'c'},
         {"launch-delay", required_argument, NULL, 'd'},
         {"use-locks", required_argument, NULL, 'l'},
         {"num-threads", required_argument, NULL, 'n'},
         {NULL, 0, NULL, 0}};
-
-    int i, c;
-    thread_data_t *data;
-    pthread_t *threads;
-    pthread_attr_t attr;
-    int nb_threads = DEFAULT_NB_THREADS;
-    use_locks = DEFAULT_USE_LOCKS;
-    int launch_delay = DEFAULT_LAUNCH_DELAY;
 
     while (1)
     {
@@ -137,16 +153,16 @@ int main(int argc, char **argv)
             printf("Options:\n");
             printf("  -h, --help\n");
             printf("        Print this message\n");
-            printf("  -c, --cs-delay <int>\n");
-            printf("        Critical section delay in cycles (default=" XSTR(DEFAULT_CS_DELAY) ")\n");
+            printf("  -c, --cs-cycles <int>\n");
+            printf("        Critical section length in cycles (default=" XSTR(DEFAULT_CS_CYCLES) ")\n");
             printf("  -d, --launch-delay <int>\n");
-            printf("        Delay between thread creations in cycles (default=" XSTR(DEFAULT_LAUNCH_DELAY) ")\n");
+            printf("        Delay between thread creations in milliseconds (default=" XSTR(DEFAULT_LAUNCH_DELAY_MS) ")\n");
             printf("  -l, --use-locks <int>\n");
             printf("        Use locks or not (default=" XSTR(DEFAULT_USE_LOCKS) ")\n");
             printf("  -n, --num-threads <int>\n");
             printf("        Number of threads (default=" XSTR(DEFAULT_NB_THREADS) ")\n");
         case 'c':
-            cs_delay = atoi(optarg);
+            cs_cycles = atoi(optarg);
             break;
         case 'd':
             launch_delay = atoi(optarg);
@@ -155,7 +171,7 @@ int main(int argc, char **argv)
             use_locks = atoi(optarg);
             break;
         case 'n':
-            nb_threads = atoi(optarg);
+            max_nb_threads = atoi(optarg);
             break;
         case '?':
             printf("Use -h or --help for help\n");
@@ -165,45 +181,52 @@ int main(int argc, char **argv)
         }
     }
 
-    printf("Nb threads : %d\n", nb_threads);
+    printf("Nb threads max : %d\n", max_nb_threads);
     printf("Use locks  : %d\n", use_locks);
 
-    if ((data = (thread_data_t *)malloc(nb_threads * sizeof(thread_data_t))) == NULL)
+    thread_data_t *data;
+    pthread_t *threads;
+
+    if ((data = (thread_data_t *)malloc(max_nb_threads * sizeof(thread_data_t))) == NULL)
     {
-        perror("malloc");
+        perror("malloc data");
         exit(1);
     }
-    if ((threads = (pthread_t *)malloc(nb_threads * sizeof(pthread_t))) == NULL)
+    if ((threads = (pthread_t *)malloc(max_nb_threads * sizeof(pthread_t))) == NULL)
     {
-        perror("malloc");
+        perror("malloc threads");
         exit(1);
     }
-
-    local_th_data = (lock_local_data *)malloc(nb_threads * sizeof(lock_local_data));
-
-    /* Catch some signals */
-    if (signal(SIGHUP, catcher) == SIG_ERR ||
-        signal(SIGINT, catcher) == SIG_ERR ||
-        signal(SIGTERM, catcher) == SIG_ERR)
+    if ((local_th_data = (lock_local_data *)malloc(max_nb_threads * sizeof(lock_local_data))) == NULL)
     {
-        perror("signal");
+        perror("malloc local_th_data");
         exit(1);
     }
 
     /* Init locks */
-#ifdef PRINT_OUTPUT
-    printf("Initializing locks\n");
-#endif
-    init_lock_global_nt(nb_threads, &the_lock);
+    DPRINT("Initializing locks\n");
+    init_lock_global_nt(max_nb_threads, &the_lock);
 
-    /* Access set from all threads */
+    pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    for (i = 0; i < nb_threads; i++)
+
+    struct timespec launch_timeout;
+    launch_timeout.tv_sec = launch_delay / 1000;
+    launch_timeout.tv_nsec = (launch_delay % 1000) * 1000000;
+
+    needed_threads = max_nb_threads;
+    gettimeofday(&start, NULL);
+
+    for (i = 0; i < max_nb_threads; i++)
     {
-#ifdef PRINT_OUTPUT
-        printf("Creating thread %d\n", i);
+#ifdef USE_HYBRIDLOCK_LOCKS
+        if (i == 48)
+            set_blocking(&the_lock, 1);
 #endif
+
+        DPRINT("Creating thread %d\n", i);
+
         data[i].id = i;
         if (pthread_create(&threads[i], &attr, test, (void *)(&data[i])) != 0)
         {
@@ -211,15 +234,30 @@ int main(int argc, char **argv)
             exit(1);
         }
 
-        cpause(launch_delay);
+        measurement();
+        nanosleep(&launch_timeout, NULL);
     }
     pthread_attr_destroy(&attr);
 
-    cpause(launch_delay * 10);
-    stop = 1;
+    for (i = 0; i < 10; i++)
+    {
+#ifdef USE_HYBRIDLOCK_LOCKS
+        if (i == 5)
+            set_blocking(&the_lock, 0);
+#endif
+
+        measurement();
+        nanosleep(&launch_timeout, NULL);
+    }
+
+    while (needed_threads-- > 0)
+    {
+        measurement();
+        nanosleep(&launch_timeout, NULL);
+    }
 
     /* Wait for thread completion */
-    for (i = 0; i < nb_threads; i++)
+    for (i = 0; i < max_nb_threads; i++)
     {
         if (pthread_join(threads[i], NULL) != 0)
         {
