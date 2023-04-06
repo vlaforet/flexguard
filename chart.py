8f4cd72c4823b1d002e1b793ce83854a4cbbbe79
vlaforet <victor.laforet@inria.fr>
2023-04-06 16:02:11 +0200

    Improved scheduling benchmark

--- chart.py
+++ chart.py
@@ -7,7 +7,7 @@ import sys
 
 def load(filename):
     thread_count, time, throughput = [], [], []
-    with open(filename) as f:
+    with open(f"tmp/{filename}") as f:
         for line in f:
             cols = line.rstrip().split(",")
             if len(cols) >= 3:
@@ -18,8 +18,11 @@ def load(filename):
 
 
 thread_count, time, hybrid = load("hybrid.csv")
-_, time_futex, futex = load("futex.csv")
+_, time_hybrid_emulated, hybrid_emulated = load("hybrid_emulated.csv")
 _, time_hybrid_spin, hybrid_spin = load("hybrid_spin.csv")
+_, time_hybrid_blocking, hybrid_blocking = load("hybrid_blocking.csv")
+_, time_mcs, mcs = load("mcs.csv")
+_, time_mutex, mutex = load("mutex.csv")
 _, time_spin, spin = load("spin.csv")
 
 if len(time) == 0:
@@ -31,12 +34,15 @@ ax.set_ylabel("Thread Count")
 ax.set_xlabel("Elapsed time")
 
 ax2 = ax.twinx()
-ax2.plot(time, hybrid, label="Hybrid", color="blue")
-ax2.plot(time_futex, futex, label="Futex", color="green")
 ax2.plot(time_hybrid_spin, hybrid_spin, label="Hybrid spin", color="orange")
-ax2.plot(time_spin, spin, label="Pure spin", color="purple")
+ax2.plot(time_hybrid_blocking, hybrid_blocking, label="Hybrid blocking", color="grey")
+ax2.plot(time_spin, spin, label="Spin", color="cyan")
+ax2.plot(time_mutex, mutex, label="Mutex", color="magenta")
+ax2.plot(time_mcs, mcs, label="MCS", color="purple")
+ax2.plot(time_hybrid_emulated, hybrid_emulated, label="Hybrid Emulated", color="green")
+ax2.plot(time, hybrid, label="Hybrid", color="blue")
 ax2.set_yscale("log")
-ax2.set_ylabel("Critical Section delay (ms)")
+ax2.set_ylabel("Critical Section time (ms)")
 
 fig.legend()
-plt.savefig("out.png")
+plt.savefig("out.png", dpi=600)
---
+++ scripts/scheduling_bench.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+DELAY=1000
+CORE_COUNT=40
+MAX_THREAD_COUNT=55
+
+cd ..
+rm -r tmp
+mkdir tmp
+
+######  Hybrid Lock
+echo Starting Hybrid Lock compilation
+make clean >/dev/null 2>/dev/null
+make LOCK_VERSION=-DUSE_HYBRIDLOCK_LOCKS >/dev/null 2>/dev/null
+
+echo Starting Hybrid Lock computation
+./scheduling -n ${MAX_THREAD_COUNT} -s -1 -d ${DELAY} > tmp/hybrid.csv
+
+######  Hybrid Lock, BPF disabled
+echo Starting Hybrid Lock (No BPF) compilation
+make clean >/dev/null 2>/dev/null
+make LOCK_VERSION=-DUSE_HYBRIDLOCK_LOCKS NOBPF=1 >/dev/null 2>/dev/null
+
+echo Starting Hybrid Lock (No BPF) emulated computation
+./scheduling -n $MAX_THREAD_COUNT -s ${CORE_COUNT} -d ${DELAY} | tee tmp/hybrid_emulated.csv
+
+echo Starting Hybrid Lock (No BPF) spin computation
+./scheduling -n ${MAX_THREAD_COUNT} -s -1 -d ${DELAY} | tee tmp/hybrid_spin.csv
+
+echo Starting Hybrid Lock (No BPF) blocking computation
+./scheduling -n ${MAX_THREAD_COUNT} -s 0 -d ${DELAY} | tee tmp/hybrid_blocking.csv
+
+###### MCS lock
+echo Starting MCS compilation
+make clean >/dev/null 2>/dev/null
+make LOCK_VERSION=-DUSE_MCS_LOCKS NOBPF=1 >/dev/null 2>/dev/null
+
+echo Starting MCS computation
+./scheduling -n ${MAX_THREAD_COUNT} -d ${DELAY} | tee tmp/mcs.csv
+
+######  lock
+echo Starting Mutex compilation
+make clean >/dev/null 2>/dev/null
+make LOCK_VERSION=-DUSE_MUTEX_LOCKS NOBPF=1 >/dev/null 2>/dev/null
+
+echo Starting Mutex computation
+./scheduling -n ${MAX_THREAD_COUNT} -d ${DELAY} | tee tmp/mutex.csv
+
+###### Spin lock
+echo Starting Spin Lock compilation
+make clean >/dev/null 2>/dev/null
+make LOCK_VERSION=-DUSE_SPINLOCK_LOCKS NOBPF=1 >/dev/null 2>/dev/null
+
+echo Starting Spin Lock computation
+./scheduling -n ${MAX_THREAD_COUNT} -d ${DELAY} | tee tmp/spin.csv
