b7648a1ddd26109dd48e1092709cab1866e8cbea
vlaforet <victor.laforet@inria.fr>
2023-06-13 17:08:36 +0200

    Handle BPF in scheduling_all.sh

--- scripts/scheduling_all.sh
+++ scripts/scheduling_all.sh
@@ -4,27 +4,36 @@ DELAY=10000
 CORE_COUNT=40
 MAX_THREAD_COUNT=45
 
-LOCKS="HYBRIDLOCK MCS FUTEX"
+LOCKS="HYBRIDLOCK HYBRIDLOCK_NOBPF MCS FUTEX"
 CONTENTIONS="0 1000 1000000"
 CACHELINES="1 5"
 
 for lock in $LOCKS;
 do
-  echo "Starting ${lock} compilation"
-  make LOCK_VERSION=-DUSE_${lock}_LOCKS NOBPF=1 clean all >/dev/null
+    switch=""
+    lock_name="${lock,,}"
+    lock_flag="${lock}"
+    bpf="NOBPF=1"
+
+  if [ "$lock" == "HYBRIDLOCK_NOBPF" ]; then
+    switch="-s ${CORE_COUNT}"
+    lock_name="hybridlock_nobpf"
+    lock_flag="HYBRIDLOCK"
+  fi
 
   if [ "$lock" == "HYBRIDLOCK" ]; then
-    switch="-s ${CORE_COUNT}"
-  else
-    switch=""
+    bpf=""
   fi
+
+  echo "Starting ${lock_name} compilation with flags switch=${switch}, bpf=${bpf}"
+  make LOCK_VERSION=-DUSE_${lock_flag}_LOCKS ${bpf} clean all >/dev/null
 
   for contention in $CONTENTIONS;
   do
     for cacheline in $CACHELINES;
     do
-      echo "Starting results/${lock,,}_c${contention}_t${cacheline}.csv"
-      stdbuf -oL ./scheduling -n ${MAX_THREAD_COUNT} $switch -d ${DELAY} -c $contention -t $cacheline > "results/${lock,,}_c${contention}_t${cacheline}.csv"
+      echo "Starting results/${lock_name}_c${contention}_t${cacheline}.csv"
+      stdbuf -oL ./scheduling -n ${MAX_THREAD_COUNT} $switch -d ${DELAY} -c $contention -t $cacheline > "results/${lock_name}_c${contention}_t${cacheline}.csv"
     done;
   done;
 done;
