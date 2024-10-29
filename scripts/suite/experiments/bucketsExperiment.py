435be343636fe4d7370ca6259e80641d1cb1d3bc
vlaforet <victor.laforet@inria.fr>
2024-10-29 17:27:00 +0100

    feat(benchmark-suite): Buckets reporting

--- scripts/suite/experiments/bucketsExperiment.py
+++ scripts/suite/experiments/bucketsExperiment.py
@@ -1,3 +1,7 @@
+import os
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 from experiments.experimentCore import ExperimentCore
 
 
@@ -33,3 +37,26 @@ class BucketsExperiment(ExperimentCore):
                         },
                     }
                 )
+
+    def report(self, results, exp_dir):
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(
+            data=results,
+            x="num-threads",
+            y="throughput",
+            hue="label",
+            style="label",
+            markers=True,
+        )
+
+        plt.title(
+            f"{results['buckets'].get(0)} Buckets Hashtable Benchmark (Higher is better)"
+        )
+        plt.xlabel("Threads")
+        plt.ylabel("Ops/s")
+        plt.grid(True)
+        plt.yscale("log")
+
+        output_path = os.path.join(exp_dir, "buckets.png")
+        plt.savefig(output_path, dpi=600, bbox_inches="tight")
+        print(f"Wrote plot to {output_path}")
