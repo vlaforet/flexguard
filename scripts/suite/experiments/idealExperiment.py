225ebd2196ae6e024063a22b5923da539b5c91d8
vlaforet <victor.laforet@inria.fr>
2024-11-29 17:09:19 +0100

    feat(benchmark-suite): idealExperiment added

---
+++ scripts/suite/experiments/idealExperiment.py
@@ -0,0 +1,99 @@
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from experiments.experimentCore import ExperimentCore
+
+
+class IdealExperiment(ExperimentCore):
+    tests = []
+
+    def __init__(self, with_debugging):
+        super().__init__(with_debugging)
+
+        locks = {
+            "MCS": "mcs",
+            "MCS-Block": "mcsblock",
+            "Pthread Mutex": "mutex",
+            "Futex": "futex",
+            "MCS-TP": "mcstp",
+        }
+
+        for label, lock in locks.items():
+            self.tests.append(
+                {
+                    "benchmark": "scheduling",
+                    "name": f"Ideal using {label} lock",
+                    "label": label,
+                    "kwargs": {
+                        "lock": lock,
+                        "latency": 1,
+                        "base-threads": 0,
+                        "num-threads": 120,
+                        "step-duration": 5000,
+                        "cache-lines": 5,
+                        "thread-step": 10,
+                        "increasing-only": 0,
+                    },
+                }
+            )
+
+    def report(self, results, exp_dir):
+        zero_rows = (results["threads"] == 0) & (results["value"] == 0)
+        max_id_per_lock = results.groupby("lock")["id"].transform(max)
+        max_id_rows = results["id"] == max_id_per_lock
+
+        clean_results = results[~(zero_rows & max_id_rows)]
+
+        agg_results = clean_results.groupby(["lock", "id"], as_index=False).agg(
+            {"value": "mean", **{c: "first" for c in clean_results.columns}}
+        )
+
+        filtered_results = agg_results[agg_results["lock"].isin(["mcs", "futex"])]
+        ideal_results = filtered_results.loc[
+            filtered_results.groupby("id")["value"].idxmin().reset_index(drop=True)
+        ]
+        ideal_results["lock"] = "ideal"
+        ideal_results["label"] = "Ideal Hybrid Lock"
+
+        full_results = pd.concat([agg_results, ideal_results], ignore_index=True)
+
+        results_ylim = full_results[
+            ~full_results["lock"].isin(["mcs", "mcsblock", "mcstp"])
+        ]
+        ax2 = sns.lineplot(
+            data=results_ylim,
+            x="id",
+            y="value",
+            hue="label",
+            style="label",
+            markers=True,
+        )
+        _, ymax = ax2.get_ylim()
+
+        plt.figure(figsize=(10, 6))
+        ax = sns.lineplot(
+            data=full_results,
+            x="id",
+            y="value",
+            hue="label",
+            style="label",
+            markers=True,
+        )
+
+        id_threads = full_results.groupby("id")["threads"].first()
+        x_ticks = ax.get_xticks()
+        ax.set_xticks(x_ticks)
+        ax.set_xticklabels([id_threads.get(t, None) for t in x_ticks])
+
+        plt.title("Single-lock Latency Microbenchmark (Lower is better)")
+        plt.xlabel("Threads")
+        plt.ylabel("Critical Section Latency (micros/cs)")
+        plt.grid(True)
+        # plt.yscale("log")
+        plt.ylim(0, ymax)
+
+        output_path = os.path.join(exp_dir, "ideal.png")
+        plt.savefig(output_path, dpi=600, bbox_inches="tight")
+        print(f"Wrote full plot to {output_path}")
