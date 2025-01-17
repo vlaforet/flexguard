3245a0350786dc2de1304b03d963bb30cc585052
vlaforet <victor.laforet@inria.fr>
2025-01-17 17:16:25 +0100

    fix(scripts): Silence test_interpose.sh

--- scripts/test_interpose.sh
+++ scripts/test_interpose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 make clean >/dev/null
-make -j40 all LOCK_VERSION=MUTEX USE_REAL_PTHREAD=1 TEST_INTERPOSE=1
-make test_interpose
+make -j40 all LOCK_VERSION=MUTEX USE_REAL_PTHREAD=1 TEST_INTERPOSE=1 >/dev/null
+make test_interpose >/dev/null
 
 ./interpose.sh ./test_interpose
