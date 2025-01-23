c9e0ca36363957c90ef731b31b385121e9ee954e
vlaforet <victor.laforet@inria.fr>
2025-01-23 17:15:13 +0100

    fix(build): Fix leveldb-1.20 build script. Removed jemalloc that was causing issues

--- scripts/build_leveldb-1.20.sh
+++ scripts/build_leveldb-1.20.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 cd ext/leveldb-1.20
-cd jemalloc/
-./autogen.sh
-./configure --without-export --disable-libdl
-make -j`nproc`
-cd ../
+# cd jemalloc/
+# ./autogen.sh
+# ./configure --without-export --disable-libdl
+# make -j$(nproc)
+# cd ../
 
 ./build_detect_platform build_config.mk .
-make -j`nproc`
+make -j$(nproc)
