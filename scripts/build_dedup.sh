3a823f76a32ba43313f7e8b9c3ef7392494334f2
vlaforet <victor.laforet@inria.fr>
2025-05-14 15:29:37 +0200

    feat(build): Better dedup build script

--- scripts/build_dedup.sh
+++ scripts/build_dedup.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
+set -e
 
 cd ext/parsec-benchmark
-./get-inputs -n
+
+if [ "$1" == "clean" ]; then
+  ./bin/parsecmgmt -a fullclean -p dedup
+  ./bin/parsecmgmt -a fulluninstall -p dedup
+  shift
+fi
+
 ./bin/parsecmgmt -a build -p dedup
-./bin/parsecmgmt -a run -p dedup -i native
+
+if [ ! -f "pkgs/kernels/dedup/run/FC-6-x86_64-disc1.iso" ]; then
+  ./get-inputs -n
+  ./bin/parsecmgmt -a run -p dedup -i native
+fi
