608b5145865e5ad7a4c6abd468019114d222cfe1
vlaforet <victor.laforet@inria.fr>
2025-05-21 17:21:03 +0200

    fix(benchmark-suite): Use splash2x's raytrace

--- scripts/build_raytrace.sh
+++ scripts/build_raytrace.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 set -e
 
-sudo apt update
-sudo apt install -y libx11-dev xorg-dev
-
 cd ext/parsec-benchmark
 
 if [ "$1" == "clean" ]; then
-  ./bin/parsecmgmt -a fullclean -p raytrace
-  ./bin/parsecmgmt -a fulluninstall -p raytrace
+  ./bin/parsecmgmt -a fullclean -p splash2x.raytrace
+  ./bin/parsecmgmt -a fulluninstall -p splash2x.raytrace
   shift
 fi
 
-./bin/parsecmgmt -a build -p raytrace
-./bin/parsecmgmt -a run -p raytrace -i simsmall
+./bin/parsecmgmt -a build -p splash2x.raytrace
+./bin/parsecmgmt -a run -p splash2x.raytrace -i simsmall
