bfc092d5980a7f598055924b44d84284a67263cc
vlaforet <victor.laforet@inria.fr>
2025-06-03 17:52:56 +0200

    feat(benchmark-suite): Change thread counts for raytrace

--- scripts/suite/experiments/raytraceExperiment.py
+++ scripts/suite/experiments/raytraceExperiment.py
@@ -9,7 +9,25 @@ class RaytraceExperiment(ExperimentCore):
     def __init__(self, locks):
         super().__init__(locks)
 
-        threads = [1, 2] + [i for i in range(10, 128, 10)] + [128]
+        threads = [
+            1,
+            2,
+            4,
+            8,
+            16,
+            32,
+            48,
+            64,
+            72,
+            102,
+            104,
+            106,
+            128,
+            192,
+            256,
+            448,
+            512,
+        ]
 
         for lock in locks:
             for t in threads:
@@ -32,7 +50,7 @@ class RaytraceExperiment(ExperimentCore):
         results_ylim = results[~results["lock"].isin(["mcs"])]
 
         plt.figure(figsize=(10, 6))
-        #plt.xlim(20, 128)
+        # plt.xlim(20, 128)
         sns.lineplot(
             data=results,
             x="threads",
