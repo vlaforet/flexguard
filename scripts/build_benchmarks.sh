5ec1f7d7dee2718c26643a6f1fa933185942a262
vlaforet <victor.laforet@inria.fr>
2025-08-20 13:55:52 +0200

    Build raytrace as the last benchmark

--- scripts/build_benchmarks.sh
+++ scripts/build_benchmarks.sh
@@ -3,8 +3,8 @@ set -e
 
 ./scripts/build_leveldb-1.20.sh $*
 ./scripts/build_index-benchmarks.sh $*
-./scripts/build_raytrace.sh $*
 ./scripts/build_streamcluster.sh $*
 ./scripts/build_dedup.sh $*
+./scripts/build_raytrace.sh $*
 
 echo "All builds completed successfully."
